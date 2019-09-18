ca22c6e9ffce0d6e96c6ed6b24638c1a61e5de95
OctoBrownie <46551461+OctoBrownie@users.noreply.github.com>
2019-09-18 12:06:18 -0700

    Update Level_Select.h

--- include/Level_Select.h
+++ include/Level_Select.h
@@ -5,8 +5,7 @@
 
 class Level_Select : public Menu {
 protected:
-    // TODO: declare Level_Select extra funcs and properties
-    int levels_per_row;
+    int rows, columns;
 
     void keyboard_input_down(const SDL_Event* event);
     int get_option_at_x_y(const int x, const int y);
@@ -14,7 +13,7 @@ public:
     Level_Select(SDL_Renderer* ren, std::vector<std::string> menu_options, int screen_height,
 			int screen_width);
 
-    int render_menu();          // override to include the formatted level array
+    int render_menu();          // override to render formatted level array
 };
 
 #endif // __Level_Select__
