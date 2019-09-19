cae5ae4604a0a904d90c3fe18bfa9bb87eb48925
OctoBrownie <46551461+OctoBrownie@users.noreply.github.com>
2019-09-19 08:28:10 -0700

    Added SDL_Rect for the levels array

--- include/Level_Select.h
+++ include/Level_Select.h
@@ -5,15 +5,16 @@
 
 class Level_Select : public Menu {
 protected:
-    int rows, columns;
+	int rows, columns;
+	SDL_Rect level_array_rect;
 
-    void keyboard_input_down(const SDL_Event* event);
-    int get_option_at_x_y(const int x, const int y);
+	void keyboard_input_down(const SDL_Event* event);
+	int get_option_at_x_y(const int x, const int y);
 public:
-    Level_Select(SDL_Renderer* ren, std::vector<std::string> menu_options, int screen_height,
-			int screen_width);
+	Level_Select(SDL_Renderer* ren, std::vector<std::string> menu_options, int screen_height,
+				int screen_width);
 
-    int render_menu();          // override to render formatted level array
+	int render_menu();          // override to render formatted level array
 };
 
 #endif // __Level_Select__
