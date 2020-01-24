3826ba8be5dbd371bdea1af1e0f33361a6b21fb1
Crystal <46551461+OctoBrownie@users.noreply.github.com>
2020-01-23 18:58:21 -0800

    General changes, added some variables to Puzzle class

--- include/Puzzle.h
+++ include/Puzzle.h
@@ -6,6 +6,12 @@
 class Puzzle {
 private:
     // TODO: declare Puzzle funcs and properties
+    int curr_x;
+    int curr_y;
+
+    int** map; // initialized as a ptr to a ptr b/c we don't know the size of array yet
+    int map_size_x;
+    int map_size_y;
 public:
     // asdf
     std::string interpret_input(const SDL_Event* event);
